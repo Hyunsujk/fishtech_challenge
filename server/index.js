acff7960dc2e7960564707fc3de16a48f66a62d9
Hyunsu Kim <hyunsujk@gmail.com>
2021-05-28 21:19:45 -0500

    initial code

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.json({ message: "test server" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
